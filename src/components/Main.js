7d46f70391cde6c8a2d294711a0b5161d9c8e271
AlexHorhat <alexhorhat@yahoo.com>
2021-03-19 11:39:04 +0200

    carousel component added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,67 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
+import Carousel from "react-elastic-carousel";
 
-const Main = ({instances, onDelete}) => {
-    const dateBuilder = (d) => {
-        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    
-        let day = days[d.getDay()];
-        let date = d.getDate();
-        let month = months[d.getMonth()];
-        let year = d.getFullYear();
-    
-        return `${day} ${date} ${month} ${year}`
-      }
+const Main = ({ instances, onDelete }) => {
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
 
-    return (
-        <main className="main-container">
-            {instances.map((instance) => (
-                <Card 
-                key={instance.id} 
-                city={`${instance.name}, ${instance.sys.country}`} 
-                temp={Math.round(instance.main.temp)} 
-                weather={instance.weather[0].main} 
-                date={dateBuilder(new Date())} 
-                low={Math.round(instance.main.temp_min)} 
-                high={Math.round(instance.main.temp_max)}
-                onDelete={onDelete}
-                instance={instance}
-                />
-            ))}
-        </main>
-    )
-}
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
 
-export default Main
+    return `${day} ${date} ${month} ${year}`;
+  };
+
+  const breakPoints = [
+      {width: 1, itemsToShow: 1 },
+      {width: 550, itemsToShow: 2 },
+      {width: 768, itemsToShow: 3 },
+      {width: 1200, itemsToShow: 5 },
+  ]
+
+  return (
+    <main className="main-container">
+      <Carousel breakPoints={breakPoints}>
+        {instances.map((instance) => (
+          <Card
+            key={instance.id}
+            city={`${instance.name}, ${instance.sys.country}`}
+            temp={Math.round(instance.main.temp)}
+            weather={instance.weather[0].main}
+            date={dateBuilder(new Date())}
+            low={Math.round(instance.main.temp_min)}
+            high={Math.round(instance.main.temp_max)}
+            onDelete={onDelete}
+            instance={instance}
+          />
+        ))}
+      </Carousel>
+    </main>
+  );
+};
+
+export default Main;
